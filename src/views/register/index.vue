0a6e21da827e15085827d6df782036d74d7395d7
baixiang2046 <1042965834@qq.com>
2020-11-16 21:47:26 +0800

    Update index.vue

--- src/views/register/index.vue
+++ src/views/register/index.vue
@@ -1,224 +1,200 @@
 <template>
-  <div> 
-    <div class="register-wrapper"> 
-      <div id="register">
-        <p class="title">注册</p>
-        <el-form
-          :model="ruleForm2"
-          status-icon
-          :rules="rules2"
-          ref="ruleForm2"
-          label-width="0"
-          class="demo-ruleForm"
-        >
-          <el-form-item prop="tel">
-            <el-input v-model="ruleForm2.tel" auto-complete="off" placeholder="请输入手机号"></el-input>
-          </el-form-item>
-          <el-form-item prop="smscode" class="code">
-            <el-input v-model="ruleForm2.smscode" placeholder="验证码"></el-input>
-            <el-button type="primary" :disabled='isDisabled' @click="sendCode">{{buttonText}}</el-button>
-          </el-form-item>
-          <el-form-item prop="pass">
-            <el-input type="password" v-model="ruleForm2.pass" auto-complete="off" placeholder="输入密码"></el-input>
-          </el-form-item>
-          <el-form-item prop="checkPass">
-            <el-input type="password" v-model="ruleForm2.checkPass" auto-complete="off" placeholder="确认密码"></el-input>
-          </el-form-item>
-          <el-form-item>
-            <el-button type="primary" @click="submitForm('ruleForm2')" style="width:100%;">注册</el-button>
-            <p class="login" @click="gotoLogin">已有账号？立即登录</p>
-          </el-form-item>
-        </el-form>
+  <div class="register-container">
+    <div class="register_box">
+      <!-- 头像区域 -->
+      <div class="avatar_box">
+        <img src="@/assets/logo.jpg" alt="">
       </div>
+      <!-- 登陆表单区域 -->
+      <el-form ref="registerFormRef" :rules="registerFormRules" :model="registerForm" label-width="0px" class="register_form">
+        <!-- 用户名 -->
+        <el-form-item prop="username" :required="true">
+          <el-input v-model="registerForm.username" prefix-icon="el-icon-user-solid" placeholder="请输入账号"></el-input>
+        </el-form-item>
+        <!-- 密码 -->
+        <el-form-item prop="pwd">
+          <el-input v-model="registerForm.pwd" prefix-icon="el-icon-lock" placeholder="请输入密码" type="password"></el-input>
+        </el-form-item>
+        <!-- 重复密码 -->
+        <el-form-item prop="repwd">
+          <el-input v-model="registerForm.repwd" prefix-icon="el-icon-lock" placeholder="请再次输入密码" type="password"></el-input>
+        </el-form-item>
+        <!-- 昵称 -->
+        <el-form-item prop="nickname">
+          <el-input v-model="registerForm.nickname" prefix-icon="el-icon-user" placeholder="请输入昵称"></el-input>
+        </el-form-item>        
+        <!-- 手机号码 -->
+        <el-form-item prop="phone">
+          <el-input v-model="registerForm.phone" prefix-icon="el-icon-phone" placeholder="请输入手机号"  onkeyup="this.value = this.value.replace(/[^\d.]/g,'');" maxlength="11" ></el-input>
+        </el-form-item>
+        <!-- 家长姓名 -->
+        <el-form-item prop="parentname">
+          <el-input v-model="registerForm.parentname" prefix-icon="el-icon-s-check" placeholder="请输入家长姓名"></el-input>
+        </el-form-item>        
+        <!-- 学校 -->
+        <el-form-item prop="school">
+          <el-input v-model="registerForm.school" prefix-icon="el-icon-school" placeholder="请输入学校"></el-input>
+        </el-form-item>  
+        <!-- 按钮区域以及登陆页面 -->
+        <el-form-item class="btns">
+          <el-button type="primary" @click="login">登陆</el-button>
+          <router-link to="/login" class="f_link"><el-link type="primary">已有账号，立即登陆</el-link></router-link>
+        </el-form-item>
+      </el-form>
     </div>
   </div>
 </template>
 
-<script> 
+<script>
+
 export default {
-  name: "Register", 
+  name: "RegisterIndex",
   data() {
-    // <!--验证手机号是否合法-->
-    let checkTel = (rule, value, callback) => {
-      if (value === '') {
-        callback(new Error('请输入手机号码'))
-      } else if (!this.checkMobile(value)) {
-        callback(new Error('手机号码不合法'))
-      } else {
-        callback()
-      }
-    }
-    //  <!--验证码是否为空-->
-    let checkSmscode = (rule, value, callback) => {
-      if (value === '') {
-        callback(new Error('请输入手机验证码'))
-      } else {
-        callback()
-      }
-    }
-    // <!--验证密码-->
-    let validatePass = (rule, value, callback) => {
-      if (value === "") {
-        callback(new Error("请输入密码"))
-      } else {
-        if (this.ruleForm2.checkPass !== "") {
-          this.$refs.ruleForm2.validateField("checkPass");
-        }
-        callback()
-      }
-    }
-    // <!--二次验证密码-->
-    let validatePass2 = (rule, value, callback) => {
-      if (value === "") {
-        callback(new Error("请再次输入密码"));
-      } else if (value !== this.ruleForm2.pass) {
-        callback(new Error("两次输入密码不一致!"));
-      } else {
-        callback();
+    return {
+      // 这是登陆表单的数据绑定对象
+      registerForm: {
+        username: '',
+        nickname: '',
+        phone: '',
+        pwd: '',
+        school: '',
+        parentname: '',
+        repwd: ''
+      },
+      // 这是表单的验证规则对象
+      registerFormRules: {
+        username: [
+          { required: true, message: '请输入账号', trigger: 'blur' },
+          {
+            validator: function(rule, value, callback) {
+              //校验中文的正则：/^[\u4e00-\u9fa5]{0,}$/
+              if (value) {
+                if (/[\u4E00-\u9FA5]/g.test(value)) {
+                    callback(new Error('不能有中文!'))
+                }else {
+                    callback()
+                }
+              }
+            },
+            trigger: "blur"
+          },
+          { min: 3, max: 10, message: '长度应在 3 到 10 个字符', trigger: 'blur' }
+        ],
+        pwd: [
+          { required: true, message: '请输入密码', trigger: 'blur' },
+          { min: 6, max: 10, message: '长度应在 6 到 10 个字符', trigger: 'blur' }
+        ],
+        repwd: [
+          { required: true, message: '请再次输入密码', trigger: 'blur' },
+          { min: 6, max: 10, message: '长度应在 6 到 10 个字符', trigger: 'blur' }
+        ],
+        nickname: [
+          { required: false, message: '请输入昵称', trigger: 'blur' },
+          { min: 2, max: 10, message: '长度应在 2 到 10 个字符', trigger: 'blur' }
+        ],
+        phone: [
+          { required: true, message: '请输入手机号', trigger: 'blur' },
+          { min: 11, max: 11, message: '长度应为 11 个数字', trigger: 'blur' }
+        ],
+        school: [
+          { required: false, message: '请输入学校', trigger: 'blur' },
+          {
+            validator: function(rule, value, callback) {
+              //校验中文的正则：/^[\u4e00-\u9fa5]{0,}$/
+              if (/^[\u4e00-\u9fa5]+$/.test(value) == false) {
+                callback(new Error("请只输入中文！"));
+              } else {
+                //校验通过
+                callback();
+              }
+            },
+            trigger: "blur"
+          },
+          { min: 2, max: 16, message: '长度应在 2 到 16 个字符', trigger: 'blur' }
+        ],
+        parentname: [
+          { required: false, message: '请输入家长姓名', trigger: 'blur' },
+          {
+            validator: function(rule, value, callback) {
+              //校验中文的正则：/^[\u4e00-\u9fa5]{0,}$/
+              if (/^[\u4e00-\u9fa5]+$/.test(value) == false) {
+                callback(new Error("请只输入中文！"));
+              } else {
+                //校验通过
+                callback();
+              }
+            },
+            trigger: "blur"
+          },
+          { min: 2, max: 16, message: '长度应在 2 到 16 个字符', trigger: 'blur' }
+        ],
       }
     };
-    return {  
-      ruleForm2: {
-        pass: "",
-        checkPass: "",
-        tel: "",
-        smscode: ""
-      },
-      rules2: {
-        pass: [{ validator: validatePass, trigger: 'change' }],
-        checkPass: [{ validator: validatePass2, trigger: 'change' }],
-        tel: [{ validator: checkTel, trigger: 'change' }],
-        smscode: [{ validator: checkSmscode, trigger: 'change' }],
-      },
-      buttonText: '发送验证码',
-      isDisabled: false, // 是否禁止点击发送验证码按钮
-      flag: true
-    }
-  }, 
+  },
   methods: {
-    // <!--发送验证码-->
-    sendCode () {
-      let tel = this.ruleForm2.tel
-      if (this.checkMobile(tel)) {
-        console.log(tel)
-        let time = 60
-        this.buttonText = '已发送'
-        this.isDisabled = true
-        if (this.flag) {
-          this.flag = false;
-          let timer = setInterval(() => {
-            time--;
-            this.buttonText = time + ' 秒'
-            if (time === 0) {
-              clearInterval(timer);
-              this.buttonText = '重新获取'
-              this.isDisabled = false
-              this.flag = true;
-            }
-          }, 1000)
-        }
-      }
-    },
-    // <!--提交注册-->
-    submitForm(formName) {
-      this.$refs[formName].validate(valid => {
-        if (valid) {
-          setTimeout(() => {
-            alert('注册成功')
-          }, 400);
-        } else {
-          console.log("error submit!!");
-          return false;
-        }
+
+    login() {
+      this.$refs.loginFormRef.validate(async valid => {
+        if (!valid) return;
+        const { data: res} = await this.$http.post("login",this.loginForm);
+        if(res.meta.status !== 200) return this.$message.error("登陆失败！");
+        this.$message.success("登陆成功");
       })
-    },
-    // <!--进入登录页-->
-    gotoLogin() {
-      this.$router.push({
-        path: "/login"
-      });
-    },
-    // 验证手机号
-    checkMobile(str) {
-      let re = /^1\d{10}$/
-      if (re.test(str)) {
-        return true;
-      } else {
-        return false;
-      }
     }
   }
 };
 </script>
 
-<style scoped>
-.loading-wrapper {
-  position: fixed;
-  top: 0;
-  right: 0;
-  left: 0;
-  bottom: 0;
-  background: #aedff8;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
-.register-wrapper img {
+<style lang="less" scoped>
+.register_box {
+  width: 450px;
+  height: 700px;
+  background-color: rgba(128, 128, 128, 0.192);
+  border-radius: 3px;
   position: absolute;
-  z-index: 1;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+
+  .avatar_box {
+    height: 120px;
+    width: 120px;
+    border: 1px solid #eee;
+    border-radius: 50%;
+    box-shadow: 0 0 5px;
+    position: absolute;
+    top: -7%;
+    left: 50%;
+    transform: translate(-50%);
+
+    img {
+      width: 100%;
+      height: 100%;
+      border-radius: 50%;
+    }
+  }
+
+  .f_link{
+      position: relative;
+      left: 30%;
+  }
 }
-.register-wrapper {
-  position: fixed;
-  top: 0;
-  right: 0;
-  left: 0;
-  bottom: 0;
+
+.register_form{
+  position: absolute;
+  bottom: 7%;
+  width: 100%;
+  padding: 0 50px;
+  box-sizing: border-box;
 }
-#register {
-  max-width: 340px;
-  margin: 60px auto;
-  background: #fff;
-  padding: 20px 40px;
-  border-radius: 10px;
-  position: relative;
-  z-index: 9;
+
+.el-button{
+  width: 100%;
 }
-.title {
-  font-size: 26px;
-  line-height: 50px;
-  font-weight: bold;
-  margin: 10px;
-  text-align: center;
+
+
+a {
+  text-decoration: none;
 }
-.el-form-item {
-  text-align: center;
-}
-.login {
-  margin-top: 10px;
-  font-size: 14px;
-  line-height: 22px;
-  color: #1ab2ff;
-  cursor: pointer;
-  text-align: left;
-  text-indent: 8px;
-  width: 160px;
-}
-.login:hover {
-  color: #2c2fd6;
-}
-.code >>> .el-form-item__content {
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-}
-.code button {
-  margin-left: 20px;
-  width: 140px;
-  text-align: center;
-}
-.el-button--primary:focus {
-  background: #409EFF;
-  border-color: #409EFF;
-  color: #fff;
-}
+
 </style>
